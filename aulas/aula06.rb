382dea9807692077efb51a8530ceda2ac0ad9f76
madusouza <ducieledesouza@gmail.com>
2015-11-06 15:19:13 -0300

    Exercicio 2

---
+++ aulas/aula06.rb
@@ -0,0 +1,4 @@
+t = Time.new(2012, 10, 20, 13, 45, 0)
+puts t.strftime('%d/%m/%Y')
+puts t.strftime('%d-%m-%Y %H:%M')
+puts t.strftime('%d de %B de %Y')
---
+++ exercicios/exercicio02.rb
@@ -0,0 +1,42 @@
+class Retangulo
+	attr_accessor :largura
+	attr_accessor :altura
+
+	def initialize(larg,alt)
+		@largura = larg
+		@altura = alt
+	end
+
+	def set_dimensoes(larg,alt)
+		if (larg.to_i > 0) and (alt.to_i > 0)
+			@largura = larg
+			@altura = alt
+		end
+	end
+
+	def area
+		@area = altura.to_i * largura.to_i
+	end
+
+	def perimetro
+		@perimetro = 2*(altura.to_i + largura.to_i)
+	end
+
+	def quadrado?
+		altura.to_i == largura.to_i
+	end
+
+	def exibir
+		puts 'largura = ' + largura.to_s 
+		puts 'altura = ' + altura.to_s
+		puts 'perimetro = ' + perimetro.to_s
+		puts 'area = ' + area.to_s
+		puts 'quadrado = ' + quadrado?.to_s
+	end
+end
+
+r = Retangulo.new(3,10)
+
+#r.set_dimensoes(3,3)
+
+r.exibir()
