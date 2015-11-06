279d42c61dc9a7a84b1f0c8312b9be261bbd6fa9
madusouza <ducieledesouza@gmail.com>
2015-11-06 19:19:00 -0300

    Exercicio 3 - organizando

--- exercicios/exercicio03.rb
+++ exercicios/exercicio03.rb
@@ -1,3 +1,24 @@
+# Programação para Redes
+# Aluna: Maria Duciele de Souza Silva
+
+# ============================
+
+# Exercício 3
+# Na classe Fracao, implemente o método to_f para retornar o valor da fração como um número real.
+
+# Na classe Fracao, implemente os seguintes métodos, todos com um argumento do tipo Fracao e retornando uma nova instância de Fracao como resultado da operação:
+
+# subtrair(f)
+
+# multiplicar(f)
+
+# dividir(f)
+
+# Na classe Fracao, implemente um método que realize a simplificação da fração. Este método deve ser utilizado sempre que a fração sofrer alguma alteração.
+
+# Escreva um programa de teste para verificar a implementação dos métodos.
+
+
 class Fracao
 	attr_accessor :numerador
 	attr_reader :denominador
@@ -14,7 +35,7 @@ class Fracao
 	end
 
 	def to_s
-	  @numerador.to_s + '/' + @denominador.to_s
+		@numerador.to_s + '/' + @denominador.to_s
 	end
 
 	def to_f
@@ -22,31 +43,31 @@ class Fracao
 	end
 
 	def somar(f)
-	  d = @denominador * f.denominador
-	  n = @numerador * f.denominador + f.numerador * @denominador
-	  #return Fracao.new(n,d)
-	  self.simplificar(n,d)
+		d = @denominador * f.denominador
+		n = @numerador * f.denominador + f.numerador * @denominador
+		#return Fracao.new(n,d)
+		self.simplificar(n,d)
 	end
 
 	def subtrair(f)
-	  d = @denominador * f.denominador
-	  n = @numerador * f.denominador - f.numerador * @denominador
-	  #return Fracao.new(n,d)
-	  self.simplificar(n,d)
+		d = @denominador * f.denominador
+		n = @numerador * f.denominador - f.numerador * @denominador
+		#return Fracao.new(n,d)
+		self.simplificar(n,d)
 	end
 
 	def multiplicar(f)
-	  d = @denominador * f.denominador
-	  n = @numerador * f.numerador
-	  #return Fracao.new(n,d)
-	  self.simplificar(n,d)
+		d = @denominador * f.denominador
+		n = @numerador * f.numerador
+		#return Fracao.new(n,d)
+		self.simplificar(n,d)
 	end
 
 	def dividir(f)
-	  d = @denominador * f.numerador
-	  n = @numerador * f.denominador
-	  #return Fracao.new(n,d)
-	  self.simplificar(n,d)
+		d = @denominador * f.numerador
+		n = @numerador * f.denominador
+		#return Fracao.new(n,d)
+		self.simplificar(n,d)
 	end
 
 	#=== Adaptado de http://codereview.stackexchange.com/a/66474
@@ -83,7 +104,7 @@ f5 = f1.multiplicar(f2)
 f6 = f1.dividir(f2)
 
 puts 'Soma: ' + f3.to_s
-puts f3.to_f
+puts f3.to_f #Numero real
 puts 'Subtracao: ' + f4.to_s
 puts f4.to_f
 puts 'Multiplicacao: ' + f5.to_s
